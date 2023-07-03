4849e171da417ceba6367fdd91208c0ce17c08e6
Sparsh-R17 <sparsh170702@gmail.com>
2023-07-03 23:58:01 +0530

    Finished: DetailScreen

--- app/src/main/java/com/example/movieapp/widgets/widgets.kt
+++ app/src/main/java/com/example/movieapp/widgets/widgets.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,6 +17,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -27,15 +29,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import com.example.movieapp.model.Movie
 import com.example.movieapp.model.getMovies
@@ -52,10 +51,10 @@ fun MovieRow(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-//            .height(150.dp)
             .clickable {
                 onItemClick(movie.id)
             },
+
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
@@ -81,7 +80,9 @@ fun MovieRow(
                     .width(250.dp)
             ) {
                 Text(
-                    text = movie.title, style = MaterialTheme.typography.headlineMedium
+                    text = movie.title,
+                    style = MaterialTheme.typography.headlineSmall,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
                 )
                 Row(
                     modifier = Modifier.fillMaxWidth(),
@@ -103,27 +104,45 @@ fun MovieRow(
                         tint = MaterialTheme.colorScheme.primary
                     )
                 }
-
                 Text(
                     text = "Released : ${movie.year}",
                     style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Normal)
                 )
                 AnimatedVisibility(visible = expanded) {
                     Column {
-                        Text(buildAnnotatedString {
-                            withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp)) {
-                                append("Plot : ")
-                            }
-                            withStyle(
-                                style = SpanStyle(
-                                    color = Color.DarkGray,
-                                    fontSize = 13.sp,
-                                    fontWeight = FontWeight.Bold
-                                )
-                            ) {
-                                append(movie.plot)
-                            }
-                        })
+                        Text(
+                            buildAnnotatedString {
+                                withStyle(
+                                    style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Normal)
+                                        .toSpanStyle()
+                                ) {
+                                    append("Plot : ")
+                                }
+                                withStyle(
+                                    style = MaterialTheme.typography.titleMedium.copy(
+                                        fontWeight = FontWeight.Light,
+                                    ).toSpanStyle()
+                                ) {
+                                    append(movie.plot)
+                                }
+                            },
+                        )
+                        Spacer(modifier = Modifier.height(5.dp))
+                        Divider()
+                        Spacer(modifier = Modifier.height(5.dp))
+                        Text(
+                            text = "Actors : ${movie.actor}",
+                            style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Normal)
+                        )
+                        Text(
+                            text = "Genre : ${movie.genre}",
+                            style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Normal)
+                        )
+                        Text(
+                            text = "Rating : ${movie.rating}",
+                            style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Normal)
+                        )
+                        Spacer(modifier = Modifier.height(5.dp))
                     }
                 }
             }
